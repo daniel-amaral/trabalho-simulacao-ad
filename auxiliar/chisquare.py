b17e8fd544369c80e3243807edb581059d633f3f
Daniel Amaral Martins <daniel.martins@radixeng.com.br>
2019-02-25 11:23:30 -0300

    Removendo arquivo chisquare nao utilizado

--- auxiliar/chisquare.py
+++
@@ -1,7 +0,0 @@
-from scipy.stats import chisquare
-
-class ChiSquare:
-
-    def tabela(self, numero_de_amostras):
-        alfa = 0.05 # intervalo de confianca de 95% definido no enunciado do trabalho
-        valor_da_tabela = chisquare()
